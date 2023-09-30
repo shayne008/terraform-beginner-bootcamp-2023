48186b4c90671054255f56467100f39bd92642a6
Heather Gibbs <hg.shayne008@gmail.com>
2023-09-30 19:44:10 +0000

    Updated main.tf

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -29,7 +29,7 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   source = var.index_html_filepath
-  content_type = "/text/html"
+  content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
@@ -60,7 +60,7 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
-  content_type = "/text/html"
+  content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
