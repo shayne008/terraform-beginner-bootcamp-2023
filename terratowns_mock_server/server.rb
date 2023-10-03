bf29f20605578f6425eb56d8a21cca0ba47932ca
Heather Gibbs <hg.shayne008@gmail.com>
2023-10-03 23:30:13 +0000

    #36 Made sure all endpoints worked correctly

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -216,10 +216,11 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
+   
 
     unless home.valid?
       error 422, home.errors.messages.to_json
@@ -240,8 +241,10 @@ class TerraTownsMockServer < Sinatra::Base
       error 404, "failed to find home with provided uuid and bearer token"
     end
 
+    # delete from mock database
+    uuid = $home[:uuid]
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
