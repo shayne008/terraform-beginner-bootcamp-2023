8f4f034b8c7b22c7723e264cdd8b3f6d77fb13d8
Heather Gibbs <hg.shayne008@gmail.com>
2023-10-08 20:43:00 +0000

    #38 Implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-# This is my first change. 
-# This is my second change.
+terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+}
 
 
 
@@ -7,7 +13,7 @@
 
 
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-terraform {
+#terraform {
 #cloud {
 #  organization = "Heather"
 #  workspaces {
@@ -15,14 +21,21 @@ terraform {
 #  }
 #}
  
+#}
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
-}
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  index_html_filepath = var.index_html_filepath
+#  error_html_filepath = var.error_html_filepath
+#  content_version = var.content_version
+#  assets_path = var.assets_path
+#}
