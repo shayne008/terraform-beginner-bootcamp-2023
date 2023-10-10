a97a704a0dbf7b9da2db9573c1cce51ba6298354
Heather Gibbs <49353061+shayne008@users.noreply.github.com>
2023-10-10 18:16:46 -0500

    38 terratowns provider (#42)
    * #38 Working in progress setting skel for custom terraform provider
    
    * #38 Implement terraform provider block in terraform
    
    * #38 Setup skel for resource
    
    * #38 Competed terraform provider

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
 #}
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
@@ -39,3 +39,14 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  assets_path = var.assets_path
 #}
+
+resource "terratowns_home" "Home" {
+  name = "How to learn Astrology in 2023!"
+  description = <<DESCRIPTION
+  This is how I would learn Astrology
+  DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "e328f4ab.cloudfront.net"
+  town = "cooker-cove"
+  content_version = 1  
+}
